758d852150f91d70f573025976682a1461f62085
Erencan Çabuk <erncncbk@gmail.com>
2022-04-25 15:54:34 +0300

    Initial Commit

---
+++ qfruit/qfruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  qfruitApp.swift
+//  qfruit
+//
+//  Created by Erencan on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct qfruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
